5112261525f780e7a222c03f6e54026c06c8548a
Ifra Malik <ifra00231@gmail.com>
2026-02-09 16:34:29 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const typed = new Typed(".auto-type", {
+  strings: [
+    "Developers",
+    "Designer",
+    "Freelance",
+    "Content Creator",
+    "Graphic Designer",
+    "Video Editor",
+    "Photographer",
+    "Animator",
+    "Illustrator",
+    "Blogger",
+    "Digital Marketer",
+    "Social Media Manager",
+    "SEO Expert",
+    "Music Producer",
+    "Game Developer",
+    "Writer",
+  ],
+  typeSpeed:100,
+  backSpeed:50,
+  loop:true,
+  backDelay:1000,
+  cursorchar:"-",
+  smartBackspace:true,
+
+});
+    
